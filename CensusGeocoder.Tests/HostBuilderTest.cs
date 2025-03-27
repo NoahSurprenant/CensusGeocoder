dd5136398a1b97028e4ed3f13b693a22fe141a2a
Noah Surprenant <noah.surprenant@gmail.com>
2025-03-27 16:50:34 -0400

    Add helper register method

---
+++ CensusGeocoder.Tests/HostBuilderTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace CensusGeocoder.Tests;
+public class HostBuilderTest
+{
+    private IHost host;
+    [SetUp]
+    public void Setup()
+    {
+        var builder = Host.CreateApplicationBuilder();
+        builder.Services.RegisterGeocodingService();
+        host = builder.Build();
+    }
+
+    [Test]
+    public void DependencyInjectionWorks()
+    {
+        using var scope = host.Services.CreateScope();
+        var service = scope.ServiceProvider.GetRequiredService<GeocodingService>();
+        Assert.Pass("Successfully injected GeocodingService");
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        host.Dispose();
+    }
+}
---
+++ CensusGeocoder/Extensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CensusGeocoder;
+public static class Extensions
+{
+    public static void RegisterGeocodingService(this IServiceCollection services)
+    {
+        services.AddHttpClient<GeocodingService>();
+    }
+}
